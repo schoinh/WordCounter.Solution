92d543b5bf55afe701e76e5d8e7c3562e0173ab0
Na Hyung Choi <schoinh@gmail.com>
2019-07-26 13:04:42 -0700

    add UI for getting word and sentence

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -3,8 +3,51 @@ using WordCounter.Models;
 
 class Program
 {
+    private static string GetWord()
+    {
+        Console.Write("\nPlease enter a word: ");
+        string userWord = Console.ReadLine();
+        bool validInput = RepeatCounter.ValidInput(userWord);
+
+        if (!validInput)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid Input: The word cannot have spaces or punctuation.");
+            Console.ResetColor();
+
+            GetWord();
+        }
+
+        return userWord;
+    }
+
+    private static string GetSentence()
+    {
+        Console.Write("\nPlease enter a sentence: ");
+        string userSentence = Console.ReadLine();
+        
+        if (userSentence.Length == 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid Input: Sentence cannot be empty.");
+            Console.ResetColor();
+
+            GetSentence();
+        }
+
+        return userSentence;
+    }
+    
     private static void Main()
     {
+        Console.Clear();
+        Console.WriteLine("------1---------5------------4---");
+        Console.WriteLine("-          WORD COUNTER         -");
+        Console.WriteLine("---3--6-------------------2------");
+        Console.WriteLine("\nFind out how many times a word appears in a sentence!");
+
+        GetWord();
+        GetSentence();
 
     }
 }
